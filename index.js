fb8747c10b518ac0c05cdcebe94b1ee016c6466b
savio <saviopf22@gmail.com>
2021-03-27 21:20:06 -0300

    *

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ const app = express();
 const port = 3333;
 
 try {
-  mongoose.connect("mongodb://127.0.0.1:27017/controle", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  mongoose.connect(
+    "mongodb://127.0.0.1:27017/controle?compressors=disabled&gssapiServiceName=mongodb",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 
   db.on("error", console.error.bind(console, "connection error:"));
   db.once("open", function () {
     console.log("database is connected");
   });
 } catch (error) {
-  console.log("err ", error);
+  console.log("err ", error); // ReferenceError: db is not defined
 }
 
 app.listen(port, () => {
