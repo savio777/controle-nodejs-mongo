41225a58b391b2b997af4128666231cfc3015129
savio <saviopf22@gmail.com>
2021-03-27 21:18:24 -0300

    erro na conexão com o banco

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
 const app = express();
 
 const port = 3333;
+
+try {
+  mongoose.connect("mongodb://127.0.0.1:27017/controle", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function () {
+    console.log("database is connected");
+  });
+} catch (error) {
+  console.log("err ", error);
+}
 
 app.listen(port, () => {
   console.log("server is running⚡️");
